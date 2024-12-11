0abd627035d5e6d0a039c74a4d062fb5529d53d8
Ginukawickramasinghe <ginukawickramasinghe@gmail.com>
2024-12-11 14:23:18 +0530

    new commit

--- XYZFirm/XYZHome/views.py
+++ XYZFirm/XYZHome/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def funding(request):
+    return render(request,'home/funding.html',{})
 # Create your views here.
+
+def funding(request):
+   
+    return render(request, 'XYZFirm/funding.html',{})
+
